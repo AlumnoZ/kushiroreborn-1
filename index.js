e8a291db220508ad05f2a38f927c2a80b460cf3d
Lawful831 <87284804+Lawful831@users.noreply.github.com>
2022-12-13 00:59:34 -0600

    Added members.json
    A way to update the puzzles

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html');
 });
 
+app.get('/api/members', (req,res)=>{
+
+  if(req.query.setmember != null){
+  switch(req.query.setmember){
+    case "tesla":
+      break;
+    case "curie":
+      break;
+    case "einstein":
+      break;
+    case "freud":
+      break;
+    case "darwin":
+      break;
+    case "lovelace":
+      break;
+    case "asimov":
+      break;      
+  }
+  }
+
+  res.sendFile(__dirname+"/members.json")
+})
+
 io.on('connection', (socket)=>{
     console.log('User connected');
     socket.on('message', (ms)=>{
