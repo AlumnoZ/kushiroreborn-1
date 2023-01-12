8c14e8718db649a6f1fa1e6143d93fdfbf3253f0
Lawful831 <87284804+Lawful831@users.noreply.github.com>
2023-01-11 23:10:38 -0600

    as

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/d', (req,res)=>{
-    if(fs.existsSync('/public/system.exe')){
+    if(fs.existsSync(__dirname+'/public/system.exe')){
       res.download('/public/system.exe');
   } else {
       res.status(404).send('File not found');
